afdf40a1f348a7e36c0b21a1ae0fba00013cf663
BernardoHer <bernaher19@gmail.com>
2022-11-25 13:45:08 -0500

    'js'

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 import data from './data.json' assert{type:'json'};
-console.log(data);
+
 let chartBarsContainer = document.querySelector('.chart_bars-container');
-git 
+
+data.forEach(element =>{
+    chartBarsContainer.innerHTML +=`
+    <div class="chart_bar">
+          <div class="chart_bar_label">$${element.amount}</div>
+          <div class="chart_bar_day">${element.day}</div>
+    </div>`
+
+});
+
+let bars =  document.querySelectorAll('.chart_bar');
+bars = [...bars]
+
+bars.forEach(bar =>{
+    bar.addEventListener('mouseover', event=>{
+        let labelElement = event.target.childNodes[1];
+        labelElement.style.display = 'block';
+        
+    });
+    bar.addEventListener('mouseout', event =>{
+        let labelElement = event.target.childNodes[1];
+        labelElement.style.display = 'none';
+
+    })
+
+})
